3eebf6a0cc91fdf666747564f54eae206c5dca3a
Peixiong Yuan <yuanpeixiong@westlake.edu.cn>
2024-12-12 10:54:02 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -9,11 +9,17 @@ panherit run-alignments \
     --threads 20
 
 panherit process-kmers \
-    --alignments /storage/yangjianLab/yuanpeixiong/SV_project/sv_heritiability/test_data/alignment/temp_alignments \
+    --alignments /storage/yangjianLab/yuanpeixiong/SV_project/sv_heritiability/test_data/alignment/test_alignments \
     --window-size 4 \
     --out /storage/yangjianLab/yuanpeixiong/pangenome_heritability/test/kmers_directory \
-    --threads 7
-
+    --threads 1
 
 
 cp -r /storage/yangjianLab/yuanpeixiong/SV_project/sv_heritiability/test_data/alignment/temp_alignments/* /data/alignment/
+
+
+panherit convert-to-plink \
+    --csv-file /storage/yangjianLab/yuanpeixiong/pangenome_heritability/test/kmers_directory/output_final_results.csv \
+    --grouped-variants /storage/yangjianLab/yuanpeixiong/pangenome_heritability/test/variants.fasta \
+    --vcf-file /storage/yangjianLab/yuanpeixiong/SV_project/pangenome_heritability/tests/test_data/test.vcf.gz \
+    --output-dir /storage/yangjianLab/yuanpeixiong/pangenome_heritability/test 
