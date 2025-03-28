825de2fafb58b5f231ed59790e3819938bd4edb8
Lostmet <101538773+Lostmet@users.noreply.github.com>
2025-03-28 22:02:36 +0800

    Update __init__.py

--- pangenome_heritability/alignment/__init__.py
+++ pangenome_heritability/alignment/__init__.py
@@ -1,11 +1,1 @@
 """Sequence alignment using MAFFT."""
-
-from .mafft_wrapper import (
-    run_alignments,
-)
-from .alignment_processor import process_alignments
-
-__all__ = [
-    'run_alignments',
-    'process_alignments'
-]
